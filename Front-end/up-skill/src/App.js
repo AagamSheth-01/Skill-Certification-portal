d7275263b33781674ac2b9d357b8ca7dacbaedf2
Aagam Sheth <aagamsheth66@gmail.com>
2025-09-20 12:46:30 +0530

    HomePage commit

--- Front-end/up-skill/src/App.js
+++ Front-end/up-skill/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
-    <div className="App">
+    <div >
+      
+      <div><HomePage/>
+      </div>
       
     </div>
   );
---
+++ Front-end/up-skill/src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './comp.css';
+
+function HomePage(){
+    return(
+        <div>
+            <h1 className="HomeTitle">UpSkill</h1>
+            <br/>
+            <div>
+                <h2>Our Teaching Parteners</h2>
+            </div>
+            
+        </div>
+    );
+}
+export default HomePage;
